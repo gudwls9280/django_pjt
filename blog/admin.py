05acc86cda0097d491c5b3fa25a4851128dab564
형진 <gudwls9280@naver.com>
2023-02-09 11:26:34 +0900

    Second commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category, Tag, Comment
+# from markdownx.admin import MarkdownxModelAdmin
+# from .models import Post, Category, Tag, Comment
+from .models import Post
+
 
 # Register your models here.
 
-admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Post)
 
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug' : ('name', )}
+# class CategoryAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug' : ('name', )}
     
-admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Category, CategoryAdmin)
 
-class TagAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug' : ('name',)}
+# class TagAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug' : ('name',)}
     
-admin.site.register(Tag, TagAdmin)
+# admin.site.register(Tag, TagAdmin)
