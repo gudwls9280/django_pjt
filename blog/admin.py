a40c43bfbb5fe7dd149f7c0f2650b0462644e7cb
형진 <gudwls9280@naver.com>
2023-02-10 11:11:42 +0900

    static 정적파일, css link, landing, about_me, 카테고리 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 # from markdownx.admin import MarkdownxModelAdmin
-# from .models import Post, Category, Tag, Comment
-from .models import Post
+from .models import Post, Category
+# from .models import Tag, Comment
 
 
 # Register your models here.
 
 admin.site.register(Post)
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     prepopulated_fields = {'slug' : ('name', )}
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )}
     
-# admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 # class TagAdmin(admin.ModelAdmin):
 #     prepopulated_fields = {'slug' : ('name',)}
